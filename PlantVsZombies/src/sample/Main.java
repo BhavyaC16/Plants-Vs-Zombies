15cfd9a3a954d629bba1188f325a6341dc1f7c94
Bhavya <bhavya18333@iiitd.ac.in>
2019-10-30 20:31:42 +0530

    initial fxml files for home page and player selection pages

--- PlantVsZombies/src/sample/Main.java
+++ PlantVsZombies/src/sample/Main.java
@@ -11,6 +11,7 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
+
         viewManager view = new viewManager();
         primaryStage = view.getMainStage();
         primaryStage.show();
