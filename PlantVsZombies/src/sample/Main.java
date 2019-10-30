0039f90f35c3640d8a4a36149fd9b989050706b7
Bhavya <bhavya18333@iiitd.ac.in>
2019-10-30 19:06:28 +0530

    Created stage, added sample button

--- PlantVsZombies/src/sample/Main.java
+++ PlantVsZombies/src/sample/Main.java
@@ -5,14 +5,14 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import sample.view.viewManager;
 
 public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        viewManager view = new viewManager();
+        primaryStage = view.getMainStage();
         primaryStage.show();
     }
 
---
+++ PlantVsZombies/src/sample/view/viewManager.java
@@ -0,0 +1,35 @@
+package sample.view;
+
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.control.Button;
+
+public class viewManager {
+
+    private static final int height = 600;
+    private static final int width = 1024;
+    private AnchorPane mainPane;
+    private Scene mainScene;
+    private Stage mainStage;
+
+    public viewManager()
+    {
+        mainPane = new AnchorPane();
+        mainScene = new Scene(mainPane, width, height);
+        mainStage = new Stage();
+        mainStage.setScene(mainScene);
+        createButtons();
+    }
+
+    public Stage getMainStage()
+    {
+        return mainStage;
+    }
+
+    private void createButtons()
+    {
+        Button button = new Button("Click me!");
+        mainPane.getChildren().add(button);
+    }
+}
