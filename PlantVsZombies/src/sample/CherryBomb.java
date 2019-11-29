8359ee58a1dc3b824c0c9d5224bd8be5263d96a1
Sonali <sonali.singhal1999@gmail.com>
2019-11-29 21:14:27 +0530

    Cherrybomb

--- PlantVsZombies/src/sample/CherryBomb.java
+++ PlantVsZombies/src/sample/CherryBomb.java
@@ -1,20 +1,50 @@
 package sample;
 
 import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.Pane;
 
 public class CherryBomb extends Plant{
+    ImageView powie=new ImageView(new Image("file:src/sample/assets/powie.gif",180,160,false,false));
     public CherryBomb(int x, int y, Pane pane, GridPane grid,int row,int col) {
-        super(x, y, "file:src/sample/assets/newCherryBomb.gif", pane, 4,200,200,grid,row,col);
-//        System.out.println(this.path);
-//        Image cImage = new Image("file:src/sample/assets/cherryBomb.gif", 150, 150, false, false);
-//        System.out.println(cImage);
-//        img.setImage(cImage);
-//        System.out.println("Adding a cherr");
+        super(x, y, "file:src/sample/assets/anim_cherrybomb.gif", pane, 4,90,68,grid,row,col);
+        powie.setX(x-40);
+        powie.setY(y-20);
+        powie.setVisible(false);
+        powie.setDisable(true);
+        pane.getChildren().add(powie);
+        attack();
+
     }
     @Override
-    public void attack(){
-        //blast all the zombies around
+    public void attack() {
+        Thread t = new Thread(() -> {
+            try {
+                Thread.sleep(1800);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            img.setVisible(false);
+            img.setDisable(true);
+            powie.setVisible(true);
+            //roast zombies
+            removepowie();
+        });
+        t.start();
+    }
+
+    public void removepowie(){
+        System.out.println("POweiieeeee");
+        Thread t = new Thread(() -> {
+            try {
+                Thread.sleep(1250);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            System.out.println("ee");
+            powie.setVisible(false);
+        });
+        t.start();
     }
 }
